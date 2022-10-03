1c962d551cdf8aa7acf4044d62917635f2930673
sentry504 <59220885+sentry504@users.noreply.github.com>
2022-10-03 12:46:37 -0600

    commit all

--- src/animes/AnimePage.js
+++ src/animes/AnimePage.js
@@ -1,29 +1,14 @@
 //Importacion del parametro establecido en la ruta de user/:id
-import {Link} from 'react-router-dom'
-import {useParams} from 'react-router-dom'
-import animes from './animesData.json'
+import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
-function AnimePage(){
-    const {id}= useParams()
-    //const data =animes.
-    const Animes= animes.map(record=>{
-        return record
-    })
-    
-    console.log(Animes)
-
-    return(
-        <>
-        <h2>{id} </h2>
-        <Link to="1">Overlord</Link><br/>
-        
-        </>
+function AnimePage() {
+    const { id } = useParams()
+    return (
+        <div>
+            <h2>{id} </h2>
+            <Link to="1">Overlord</Link><br />
+        </div>
     )
 }
 export default AnimePage
-
-/*<>{animes && animes.map( record=>{
-    record.volumen.capitulos.forEach(element => {
-        return(<ol>{element}</ol>)
-    });
-})}</>*/
